8ef086ef58a494648b692bae4a3947255c95d5e9
Robin Erlacher <rerlache@gmail.com>
2023-03-26 16:35:50 +0200

    WiP day 3

---
+++ 03_Day_Setting_Up/exercise/src/index.js
@@ -0,0 +1,91 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import htmlLogo from "./images/html.svg";
+import cssLogo from "./images/css.svg";
+import jsLogo from "./images/javascript.svg";
+import reactLogo from "./images/react.svg";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+const logos = [htmlLogo, cssLogo, jsLogo, reactLogo];
+const logosFormatted = logos.map((logo) => (
+  <img key={logo} src={logo} className="logomargin" />
+));
+const header = (
+  <header>
+    <div className="header-wrapper">
+      <center>
+        <h3>Front End Technologies</h3>
+        <div>{logosFormatted}</div>
+      </center>
+    </div>
+  </header>
+);
+const subHeader = "Subscribe";
+const subDescription = "Sign up with your email to receive news and updates";
+
+const subscribeCard = (
+  <div className="subscribecardstyle">
+    <h1 className="subheaderstyle">{subHeader.toUpperCase()}</h1>
+    <p className="subdescriptionstyle">{subDescription}</p>
+    <div>
+      <input
+        id="firstNameInput"
+        placeholder="First name"
+        className="subinputstyle"
+      />
+      <input
+        id="lastNameInput"
+        placeholder="Last name"
+        className="subinputstyle"
+      />
+      <input id="emailInput" placeholder="Email" className="subinputstyle" />
+    </div>
+    <button className="subbuttonstyle">{subHeader}</button>
+  </div>
+);
+
+const person = {
+  firstName: "Robin",
+  lastName: "Erlacher",
+  jobTitle: "Junior Developer",
+  skills: [
+    "HTML",
+    "CSS",
+    "C#",
+    "react",
+    "DevOps",
+    "powershell",
+    "power platform",
+    "javascript",
+    "english",
+    "german",
+  ],
+  joined: "01.12.2022",
+  photo: "",
+  getFullName() {
+    return this.firstName + " " + this.lastName;
+  },
+};
+
+const userCard = (
+  <div>
+    <img src={person.photo} alt={person.getFullName()} />
+    <h1>{person.getFullName().toUpperCase()}</h1>
+    <p>{person.jobTitle}</p>
+    <h3>Skills</h3>
+    <p>{person.skills.join(", ")}</p>
+    <p>Joined on {person.joined}</p>
+  </div>
+);
+
+const app = (
+  <div className="app">
+    {header}
+    {subscribeCard}
+    {userCard}
+  </div>
+);
+
+root.render(app);
---
+++ 03_Day_Setting_Up/index.js
@@ -0,0 +1,7 @@
+import doSomeMath from "./math.js"
+import { addTwo, subtract, multiply } from "./math.js"
+import * as everything from "./math.js"
+
+console.log(addTwo(5,5))
+console.log(doSomeMath.addTwo(4,4))
+console.log(everything)
